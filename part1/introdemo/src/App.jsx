0a0d143dec4b68ecc084a15e76a71f26fe59ed1a
kevinscheffler <27833332+KevinScheffler@users.noreply.github.com>
2025-04-12 11:53:21 -0400

    Initial

---
+++ part1/introdemo/src/App.jsx
@@ -0,0 +1,24 @@
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log(now, a + b)
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  )
+}
+
+export default App
